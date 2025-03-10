19352f0e21571dd669a13daff4b8fe4bf29229a5
aziz2737222 <azizmzeh5@gmail.com>
2025-03-10 14:00:51 +0100

    Modif dossier.py

--- dossier.py
+++ dossier.py
@@ -1,5 +1,4 @@
-def somme(T):
-    S=0
-    for t in T:
-        S+=t
-    return S
+Data=[1,3,5]
+print('la somme est :', sum(Data))
+print('le min est :', min(Data))
+print('le max est :', max(Data))
